88c8db9559b03cb3b637c4eafe04a97322612dc7
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-10-16 22:27:12 +0530

    Added Enum Example

---
+++ namastejava/src/namastejava/EnumExample.java
@@ -0,0 +1,12 @@
+package namastejava;
+
+public class EnumExample {
+
+	public enum direction{
+		NORTH,SOUTH, EAST, WEST
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(direction.NORTH);
+	}
+}
