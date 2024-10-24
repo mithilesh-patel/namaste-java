9aa52a9fdffcef0bb64f7ccec6c69c5210666376
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-10-24 22:57:30 +0530

    Priority queue added

--- namastejava/src/namastejava/PriorityQueueExample.java
+++ namastejava/src/namastejava/PriorityQueueExample.java
@@ -5,25 +5,28 @@ import java.util.*;
 public class PriorityQueueExample {
 	public static void main(String args[]) {
 		PriorityQueue<Integer> pq = new PriorityQueue<>();
-//		pq.add(12);
-//		pq.add(16);
-//		pq.add(69);
-//		pq.add(32);
-//		pq.add(71);
-//		pq.add(100);
-//		
-//		pq.add(71);
-//		pq.add(111);
-//
-//		pq.remove(71);
+		pq.offer(12);
+		pq.offer(16);
+		pq.offer(69);
+		pq.offer(32);
+		pq.offer(71);
+		pq.offer(100);
 		
-		System.out.println(pq.element());
+		pq.offer(71);
+		pq.offer(111);
+
+		pq.remove(71);
+		pq.poll();
 
 		Iterator<Integer> iq = pq.iterator();
 
 		while (iq.hasNext()) {
 			System.out.println(iq.next());
 		}
+		
+		System.out.println("=====================");
+		
+		System.out.println(pq.element());
 
 	}
 }
