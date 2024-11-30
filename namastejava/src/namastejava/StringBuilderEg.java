2ba5d227dd1af01d3cb9289007930b0a586a98ba
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-11-30 16:48:26 +0530

    Stringbuilder class created

---
+++ namastejava/src/namastejava/StringBuilderEg.java
@@ -0,0 +1,12 @@
+package namastejava;
+
+public class StringBuilderEg {
+	public static void main(String[] args) {
+		StringBuilder sb1 = new StringBuilder();
+		StringBuilder sb2 = new StringBuilder();
+		StringBuilder sb3 = sb1.append("Mithilesh");
+		
+		System.out.println(sb1==sb2); // False
+		System.out.println(sb3==sb1); // True
+	}
+}
