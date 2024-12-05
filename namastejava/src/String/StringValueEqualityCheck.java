f4b09fc996dee3d2d2234af7edb9423f5927b9af
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-12-05 23:59:23 +0530

    String Value Equality Check

---
+++ namastejava/src/String/StringValueEqualityCheck.java
@@ -0,0 +1,15 @@
+package String;
+
+public class StringValueEqualityCheck {
+	public static void main(String[] args) {
+		String a= "Apple";
+		String b= "Ball";
+		
+		if(a.equals(b)) {
+			System.out.println("a and b are equal");
+		}else {
+			System.out.println("a and b are NOT equal");
+			
+		}
+	}
+}
