65694207b13de9200a96c0dc78a488f00e63c727
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-12-06 21:21:07 +0530

    String Functions file created

---
+++ namastejava/src/String/StringFunctions.java
@@ -0,0 +1,11 @@
+package String;
+
+public class StringFunctions {
+
+	public static void main(String[] args) {
+		String name = "Mithilesh Kumar Patel";
+		System.out.println("Length of 'name' is "+ name.length());
+		System.out.println("ChatAt at index 5 is "+ name.charAt(5));
+		
+	}
+}
