8787fefc2b2876d6178b65eaa5280ac1232f4c81
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-12-07 17:13:49 +0530

    Array package created

---
+++ namastejava/src/Array/array.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class array {
+	public static void main(String[] args) {
+		
+//		All the 4 below can create arrays
+		
+		int[] numbers1 = new int[3];
+		int [] numbers2 = new int[3];
+		int numbers3[] = new int[3];
+		int numbers4 [] = new int[3];
+		
+		System.out.println(numbers1.length);
+		System.out.println(numbers2.length);
+		System.out.println(numbers3.length);
+		System.out.println(numbers4.length);
+	}
+}
