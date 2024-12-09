b76d7f543295da914b60918349906a4c594490c2
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-12-09 08:49:08 +0530

    Integer Wrapper Class

--- namastejava/src/WrapperClasses/IntegerWrapperClass.java
+++ namastejava/src/WrapperClasses/IntegerWrapperClass.java
@@ -14,9 +14,29 @@ public class IntegerWrapperClass {
 		int j = i;
 		System.out.println(j);
 		
+		
+//		Converting int to Integer 
+		int x= 456;
+		Integer X= Integer.valueOf(x);
+		int y = Integer.parseInt("345");
+		System.out.println("X- Integer : "+ X);
+		System.out.println("y- int: "+ y);
+		
+		
+		
+		
+		
+		
+//		==============================================================
+		
+//		How to check the type of the instance?
+
 		List<String> ls = new ArrayList<String>();
 		
 		System.out.println(i.getClass().getName());
 		System.out.println(ls.getClass().getName());
+		
+		
+		
 	}
 }
