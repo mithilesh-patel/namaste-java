c53f9832f6ff36fc02de58405f38beccc75c0976
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-12-09 20:39:52 +0530

    Convert Array to ArrayList

---
+++ namastejava/src/ArrayList/ArrayToArrayList.java
@@ -0,0 +1,16 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class ArrayToArrayList {
+	public static void main(String[] args) {
+		String[] birds = {"sparrow", "parrot", "hawk"};
+		
+		ArrayList<String> BirdsArrayList = new ArrayList<String>();
+		Collections.addAll(BirdsArrayList, birds);
+		
+		System.out.println("BirdsArrayList : " + BirdsArrayList);
+	}
+}
