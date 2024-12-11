8b88c11b055858705bd4c3c3cd5b3f5e70d89fe5
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-12-11 22:36:48 +0530

    Manipulating Date and Time

---
+++ namastejava/src/DateTime/ManipulateDateTime.java
@@ -0,0 +1,17 @@
+package DateTime;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
+public class ManipulateDateTime {
+	public static void main(String[] args) {
+		LocalDate date = LocalDate.of(1997, 10, 27);
+		LocalTime time = LocalTime.of(10,23,23, 0);
+		
+//		Combining Date and Time
+		LocalDateTime dateTime1 = LocalDateTime.of(date, time);
+		System.out.println(dateTime1);
+	}
+}
