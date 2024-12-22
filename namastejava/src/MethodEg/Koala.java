2e25352a2360963025d9c9454b6224b2e25fddd6
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-12-22 19:09:12 +0530

    Static main method calling

---
+++ namastejava/src/MethodEg/Koala.java
@@ -0,0 +1,8 @@
+package MethodEg;
+
+public class Koala {
+	public static int count =234;
+	public static void main(String[] args) {
+		System.out.println(count);
+	}
+}
---
+++ namastejava/src/MethodEg/KoalaTester.java
@@ -0,0 +1,7 @@
+package MethodEg;
+
+public class KoalaTester {
+	public static void main(String[] args) {
+		Koala.main(args);
+	}
+}
