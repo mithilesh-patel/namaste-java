de9c77cf35138d33ae05feb310cce771293f126b
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-12-24 00:14:16 +0530

    Static Imports eg

--- namastejava/src/DateTime/DataExtractionDate.java
+++ namastejava/src/DateTime/DataExtractionDate.java
@@ -3,10 +3,21 @@ package DateTime;
 import java.time.LocalDate;
 
 public class DataExtractionDate {
+
+	public static void StaticMethodEg() {
+		System.out.println("This is example of Static imports");
+	}
+	
+	public void InstanceMethod() {
+		System.out.println("This is example of Instance imports");
+		
+	}
+
 	public static void main(String[] args) {
+
 		LocalDate date = LocalDate.of(1997, 10, 27);
-		
+
 		System.out.println(date.getDayOfMonth());
-		
+
 	}
 }
