535a1154e6484a943be3b1416dbda4d8476f18da
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-12-28 11:34:56 +0530

    Overloading Methods

---
+++ namastejava/src/OverloadingMethods/overloadingVarargs.java
@@ -0,0 +1,30 @@
+package OverloadingMethods;
+
+public class overloadingVarargs {
+
+//	static int multiply(int... x) {
+//		int mul = 1;
+//		for (int a : x) {
+//			mul = a * mul;
+//		}
+//		return mul;
+//	}
+	
+	static int multiply(int[] x) {
+	    int mul;
+	    		
+		for (int i : x) {
+			mul  = mul * i;
+		}
+		
+		return mul;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
