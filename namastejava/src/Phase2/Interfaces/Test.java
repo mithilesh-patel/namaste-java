7b8f39ba5b5607a01f370de46b9ce54b3bb84206
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2025-08-07 13:39:42 +0530

    OCA:Ch-1 Comment multiline

---
+++ namastejava/src/Phase2/Interfaces/Test.java
@@ -0,0 +1,17 @@
+package Phase2.Interfaces;
+
+
+/*
+jlkfd
+slkfslkfdj
+lksjf
+
+This is a multiline comment
+every line does not need to start with a *
+*/
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
